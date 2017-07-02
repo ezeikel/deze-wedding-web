6cb02162474578c92f2c4603c6e4d5ba3ec67aec
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-07-03 00:42:45 +0100

    Added header component to replace nav component. Removed site logo as component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { DezeLogoComponent } from './deze-logo/deze-logo.component';
 import { HamburgerComponent } from './hamburger/hamburger.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DezeLogoComponent,
     HamburgerComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
