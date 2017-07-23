250bcd3eb41bcf3cd77d786cd88e274b2f96f476
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-07-23 22:16:58 +0100

    Using css variables. Amending header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HamburgerComponent } from './hamburger/hamburger.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
 
@@ -13,7 +12,6 @@ import { CarouselComponent } from './carousel/carousel.component';
   declarations: [
     AppComponent,
     HamburgerComponent,
-    NavBarComponent,
     HeaderComponent,
     CarouselComponent
   ],
