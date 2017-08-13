f176accbcbb5c01b573db979483f1e95cace4c7a
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-08-13 20:14:44 +0100

    Implemented hamburger menu.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  mobileMenuActive: boolean;
 
-  constructor() { }
+  constructor() {
+    this.mobileMenuActive = false;
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuActive = !this.mobileMenuActive;
+  }
 
   ngOnInit() {
   }
