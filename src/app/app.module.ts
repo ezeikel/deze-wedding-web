03fc777a3786762dd9b26050ffa2cf72129ac4d6
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2019-09-25 01:37:45 +0100

    Updating Angular to try a fix borked project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
